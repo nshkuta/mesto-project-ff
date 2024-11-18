6f2aa5174a97440cb9b6f36dbc56189a9fc4ee31
Nikolay Shkuta <accs@shkuta.ru>
2024-11-18 20:50:17 +0300

    1test

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+function createCard(card, deleteFunc) {
+  let cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  cardElement.querySelector(".card__image").src = card["link"];
+  cardElement.querySelector(".card__image").alt = card["name"];
+  cardElement.querySelector(".card__title").textContent = card["name"];
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", deleteFunc);
+  return cardElement;
+}
 
-// @todo: Функция удаления карточки
+// Функция удаления карточки
+function deleteCard(event) {
+  event.target.parentElement.remove();
+}
 
-// @todo: Вывести карточки на страницу
+// Вывести карточки на страницу
+placesList = document.querySelector(".places__list");
+
+initialCards.forEach((card) => {
+  placesList.append(createCard(card, deleteCard));
+});
